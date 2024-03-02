5ae9f9fb98542592c8100b17b41616997452ae4d
Blo <p.yu.mihno@inbox.ru>
2024-03-02 21:21:24 +0300

    upd: domain done

---
+++ internal/domain/YoutubeApi.go
@@ -0,0 +1,24 @@
+package domain
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func GetThumbnail(videoId string) ([]byte, error) {
+	resp, err := http.Get(fmt.Sprintf("https://img.youtube.com/vi/%s/hqdefault.jpg", videoId))
+	if err != nil {
+		log.Println("Error while downloading video with id - ", videoId)
+		return nil, fmt.Errorf("Error while downloading video | Video id - %s", videoId)
+	}
+
+	thumblenailBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("Error while reading bytes from video with id - ", videoId)
+		return nil, fmt.Errorf("Error while read response | Video id - %s", videoId)
+	}
+
+	return thumblenailBytes, nil
+}
